25225bbd0c5bcb22c85576aed4893269b7b8ffb3
Khalid Talakshi <khalid.talakshi@outlook.com>
2021-08-14 12:02:32 -0400

    changed over to apolo-server-express

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolvers";
+import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const app = express();
 
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-apolloServer
-  .listen()
-  .then(({ url }) => console.log(`Server is running on ${url}`));
+await apolloServer.start();
+
+apolloServer.applyMiddleware({
+  app,
+  path: "/graphql",
+});
+
+app.listen(4000, () => {
+  console.log(`Server is running`);
+});
