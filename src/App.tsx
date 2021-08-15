09b23ae3f2437eeaec65be464d87668538761652
Khalid Talakshi <khalid.talakshi@outlook.com>
2021-08-14 20:38:05 -0400

    added dark theme via material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,47 @@
 import React from "react";
 import { Switch, Route, BrowserRouter as Router, Link } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { createTheme, CssBaseline, ThemeProvider } from "@material-ui/core";
 import "./App.css";
 
 import { RegistrationForm } from "./components";
 
 function App() {
+  const client = new ApolloClient({
+    uri: "https://6642b518cdba.ngrok.io/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  const darkTheme = createTheme({
+    palette: {
+      type: "dark",
+    },
+  });
+
   return (
-    <Router>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/register">Register</Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route exact path="/register">
-          <RegistrationForm />
-        </Route>
-        <Route exact path="/">
-          <p>Hello World!</p>
-        </Route>
-      </Switch>
-    </Router>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Router>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/register">
+              <RegistrationForm />
+            </Route>
+            <Route exact path="/">
+              <p>Hello World!</p>
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const TEST_QUERY = gql`
+  query ExampleQuery {
+    info
+  }
+`;
 
 export const RegistrationForm = () => {
+  const { data } = useQuery(TEST_QUERY);
+
+  console.log(data);
   return <p>Register</p>;
 };
