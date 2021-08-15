89f5cebe8d01af7180d8db4b8d8dd6ce6f64e57a
Khalid Talakshi <khalid.talakshi@outlook.com>
2021-08-14 20:18:52 -0400

    added RegistrationForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from "react";
-import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route, BrowserRouter as Router, Link } from "react-router-dom";
 import "./App.css";
+
+import { RegistrationForm } from "./components";
 
 function App() {
   return (
     <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
       <Switch>
         <Route exact path="/register">
-          <p>Register</p>
+          <RegistrationForm />
         </Route>
         <Route exact path="/">
           <p>Hello World!</p>
---
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const RegistrationForm = () => {
+  return <p>Register</p>;
+};
